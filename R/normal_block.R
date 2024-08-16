7aaeac1dd29d9a777774eefe227fe864570edec5
jea312 <jeanne.tous@gmail.com>
2024-08-16 16:06:14 +0200

    added general functionf or user to get appropriate model

---
+++ R/normal_block.R
@@ -0,0 +1,56 @@
+#' Normal-block model
+#'
+#' Fit a normal-block model with a variational algorithm
+#' @param Y observations data, n observations of p categories, dim n*p.
+#' @param X covariates data, n observations of d covariates, dim n*d.
+#' @param nb_blocks number of blocks (if fixed) or list of possible number of blocks otherwise
+#' @param C blocks, if known, under the form of a p * Q matrix
+#' @param sparsity sparsity factor to apply to blocks covariance matrix (or possibly list of such values with same length as nb_blocks)
+#' @param zero_inflation boolean to indicate if Y is zero-inflated and adjust fitted model as a consequence
+#' @param niter number of iterations in model optimization
+#' @param threshold loglikelihood / elbo threshold under which optimization stops
+#' @return an R6 object with class [`NB`] or [`NB_unknown`] or [`NB_unknown_ZI`]
+#' @export
+normal_block <- function(Y, X, nb_blocks = NULL, blocks = NULL, sparsity = 0,
+                         zero_inflation = FALSE, niter = 100,
+                         threshold = 1e-4) {
+  if(! is.null(blocks)){
+    if(zero_inflation){
+      model <- NB_fixed_blocks_zi$new(Y, X, blocks, sparsity = sparsity,
+                                      niter = niter, threshold = threshold)
+      model$optimize()
+      return(model)
+    }else{
+      model <- NB_fixed_blocks$new(Y, X, blocks, sparsity = sparsity,
+                                   niter = niter, threshold = threshold)
+      model$optimize()
+      return(model)
+    }
+  }
+
+  if(length(nb_blocks) == 1){
+    if(zero_inflation){
+      model <- NB_fixed_Q_zi$new(Y, X, nb_blocks, sparsity = sparsity,
+                                      niter = niter, threshold = threshold)
+      model$optimize()
+      return(model)
+    }else{
+      model <- NB_fixed_Q$new(Y, X, nb_blocks, sparsity = sparsity,
+                              niter = niter, threshold = threshold)
+      model$optimize()
+      return(model)
+    }
+  }
+
+  if(zero_inflation){
+    models <- NB_unknown_zi$new(Y, X, nb_blocks, sparsity = sparsity,
+                               niter = niter, threshold = threshold)
+    models$optimize()
+    return(models)
+  }else{
+    models <- NB_unknown$new(Y, X, nb_blocks, sparsity = sparsity,
+                            niter = niter, threshold = threshold)
+    models$optimize()
+    return(models)
+  }
+}
