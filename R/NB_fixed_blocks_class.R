6fd50cf0682133242f4270e0c79b6b62edbecfd4
Julien Chiquet <julien.chiquet@inrae.fr>
2024-09-06 15:07:14 +0200

    various simplification for fixed block, and potential fixes in the loglikelihood

--- R/NB_fixed_blocks_class.R
+++ R/NB_fixed_blocks_class.R
@@ -50,9 +50,9 @@ NB_fixed_blocks <- R6::R6Class(
   ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   private = list(
     gamma   = NA, # variance of  posterior distribution of W
-    mu      = NA,  #  mean for posterior distribution of W
+    mu      = NA, #  mean for posterior distribution of W
 
-    compute_loglik  = function(B, dm1, omegaQ, gamma, mu) {
+    compute_complete_loglik  = function(B, dm1, omegaQ, gamma, mu) {
       R   <- self$Y - self$X %*% B
       log_det_omegaQ <- as.numeric(determinant(omegaQ, logarithm = TRUE)$modulus)
       dm1C   <- (dm1 * self$C)
@@ -61,44 +61,71 @@ NB_fixed_blocks <- R6::R6Class(
 
       J <- - .5 * self$n * (self$p + self$Q) * log(2 * pi) + .5 * self$n * sum(log(dm1))
       J <- J - .5 * sum(R %*% (dm1 * t(R))) + sum(R %*%  dm1C %*% t(mu))
-      J <- J - .5 * self$n * sum(diag(Ctdm1C %*% gamma))
-      J <- J - .5 * sum(diag(Ctdm1C %*% mutmu))
+      J <- J - .5 * self$n * sum(diag(Ctdm1C %*% (gamma + mutmu/self$n)))
       J <- J + .5 * self$n * log_det_omegaQ
-      J <- J - .5 * self$n * sum(diag(omegaQ %*% gamma))
-      J <- J - .5 * sum(diag(omegaQ %*% mutmu))
+      ## when not sparse, this terms equal -n Q /2 by definition of OmegaQ_hat
+      if (self$sparsity > 0) {
+        J <- J - .5 * self$n * sum(diag(omegaQ %*% (gamma + mutmu/self$n)))
+      } else {
+        J <- J - .5 * self$n * self$Q
+      }
       if (self$sparsity > 0) {
         J - self$sparsity * sum(abs(self$sparsity_weights * omegaQ))
       }
       J
     },
 
+    compute_loglik  = function(B, dm1, omegaQ, gamma, mu) {
+      R   <- self$Y - self$X %*% B
+      log_det_omegaQ <- as.numeric(determinant(omegaQ, logarithm = TRUE)$modulus)
+      log_det_gamma  <- as.numeric(determinant(gamma, logarithm = TRUE)$modulus)
+      dm1C   <- (dm1 * self$C)
+      Ctdm1C <- t(self$C) %*% dm1C
+      mutmu  <- t(mu) %*% mu
+      muCT  <-  mu %*% t(self$C)
+
+      # I have a problem with these two terms... not sure to understand what they correspond to
+      # J <- J - .5 * sum(R %*% (dm1 * t(R))) + sum(R %*%  dm1C %*% t(mu))
+      # J <- J - .5 * self$n * sum(diag(Ctdm1C %*% (gamma + mutmu/self$n)))
+      # Should be equivalent to the following (which exactly equal, by definition of d_hat, -np/2)
+      ## - .5 * self$n * sum(diag( dm1 * (crossprod(R - muCT)/self$n + self$C %*% gamma %*% t(self$C))))
+      J <- -.5 * self$n * self$p * log(2 * pi * exp(1)) + .5 * self$n * sum(log(dm1)) + .5 * self$n * log_det_omegaQ
+      if (self$sparsity > 0) {
+        J <- J - .5 * self$n * sum(diag(omegaQ %*% (gamma + mutmu/self$n)))
+        J - self$sparsity * sum(abs(self$sparsity_weights * omegaQ))
+      }
+      J <- J + .5 * self$n * log_det_gamma
+      J
+    },
+
     EM_initialize = function() {
       B      <- private$XtXm1 %*% t(self$X) %*% self$Y
-      R      <- t(self$Y - self$X %*% B)
-      dm1    <- 1 / check_one_boundary(check_zero_boundary(diag(cov(t(R)))))
+      R      <- self$Y - self$X %*% B
+      dm1    <- 1 / check_one_boundary(check_zero_boundary(diag(cov(R))))
       gamma  <- solve(t(self$C) %*% (dm1 * self$C))
-      mu     <- t(gamma %*% t(self$C) %*% (dm1 * R))
-      omegaQ <- solve(gamma + (1 / self$n) * t(mu) %*% mu)
+      mu     <- R %*% (dm1 * self$C) %*% gamma
+      omegaQ <- solve(gamma + (1 / self$n) * crossprod(mu))
       list(B = B, dm1 = dm1, omegaQ = omegaQ, gamma = gamma, mu = mu)
     },
 
     EM_step = function(B, dm1, omegaQ, gamma, mu) {
-      R      <- t(self$Y - self$X %*% B)
+
+      R <- self$Y - self$X %*% B
 
       ## E step
-      gamma  <- solve(omegaQ + t(self$C) %*% (dm1 * self$C))
-      mu     <- t(gamma %*% t(self$C) %*% (dm1 * R))
+      gamma <- solve(omegaQ + diag(colSums(dm1 * self$C)))
+      mu <- R %*% (dm1 * self$C) %*% gamma
 
       ## M step
-      B      <- private$XtXm1 %*% t(self$X) %*% (self$Y - mu %*% t(self$C))
-      ddiag  <- (1 / self$n) * (diag(R %*% t(R)) - 2 * diag(R %*% mu %*% t(self$C)))
-      ddiag  <- ddiag + (1 / self$n) * diag(self$C %*% t(mu) %*% mu %*% t(self$C))
-      ddiag  <- ddiag + diag(self$C %*% gamma %*% t(self$C))
-      dm1    <- as.vector(1 / ddiag)
+      muCT  <- mu %*% t(self$C)
+      B     <- private$XtXm1 %*% crossprod(self$X, self$Y - muCT)
+      ddiag <- colMeans((R - muCT)^2) + diag(self$C %*% gamma %*% t(self$C))
+      dm1   <- as.vector(1 / ddiag)
+      sigma_hat <- gamma + (1 / self$n) * crossprod(mu)
+
       if (self$sparsity == 0) {
-        omegaQ <- solve(gamma + (1 / self$n) * t(mu) %*% mu)
-      }else {
-        sigma_hat <- gamma + (1 / self$n) * t(mu) %*% mu
+        omegaQ <- solve(sigma_hat)
+      } else {
         glasso_out <- glassoFast::glassoFast(sigma_hat, rho = self$sparsity * self$sparsity_weights)
         if (anyNA(glasso_out$wi)) stop("GLasso fails")
         omegaQ <- Matrix::symmpart(glasso_out$wi)
