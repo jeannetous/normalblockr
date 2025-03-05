074523b9077d7c377459076e963061cdc4bc048e
Julien Chiquet <julien.chiquet@inrae.fr>
2025-03-05 11:15:23 +0100

    commenting element_per_cluster which strangely introduce failure in quarto rendering

--- R/NB_class.R
+++ R/NB_class.R
@@ -231,8 +231,8 @@ NB <- R6::R6Class(
     #' @field get_res_covariance whether the residual covariance is diagonal or spherical
     get_res_covariance = function(value) private$res_covariance,
     #' @field clustering given as the list of elements contained in each cluster
-    clustering = function(value) get_clusters(private$C),
-    #' @field elements_per_cluster given as the list of elements contained in each cluster
-    elements_per_cluster = function(value) split(names(self$clustering), self$clustering)
+    clustering = function(value) get_clusters(private$C)
+    #' #' @field elements_per_cluster given as the list of elements contained in each cluster
+    #' elements_per_cluster = function(value) split(names(self$clustering), self$clustering)
   )
 )
