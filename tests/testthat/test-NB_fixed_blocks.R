8185f3793dc0a1ec44ee706cf71de05b14fcaa95
Julien Chiquet <julien.chiquet@inrae.fr>
2025-03-26 09:30:54 +0100

    added the possibility to change sparsity and optimize afterwards

--- tests/testthat/test-NB_fixed_blocks.R
+++ tests/testthat/test-NB_fixed_blocks.R
@@ -11,15 +11,20 @@ C <- testdata$parameters$C
 test_that("normal block with diagonal residual covariance and known clusters", {
   data <- normal_data$new(Y, X)
 
-  model <- NB_fixed_blocks$new(data, C)
+  model <- normalblockr:::NB_fixed_blocks$new(data, C)
   model$optimize()
   expect_gt(model$loglik, -2600)
   expect_lt(Metrics::rmse(model$fitted, Y), 1)
 
-  model <- NB_fixed_blocks$new(data, C, sparsity = 0.05)
+  model_sparse <- normalblockr:::NB_fixed_blocks$new(data, C, sparsity = 0.05)
+  model_sparse$optimize()
+  expect_gt(model_sparse$loglik, -2600)
+  expect_lt(Metrics::rmse(model_sparse$fitted, Y), 1)
+
+  model$sparsity <- 0.05
   model$optimize()
-  expect_gt(model$loglik, -2600)
-  expect_lt(Metrics::rmse(model$fitted, Y), 1)
+  expect_equal(model_sparse$loglik, model$loglik, tolerance = 1e-2)
+  expect_equal(model_sparse$model_par$OmegaQ, model$model_par$OmegaQ, tolerance = 1e-2)
 
 })
 
